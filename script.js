044e7ce836220fba9d8ff471c63c3e30d7270300
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-07-08 15:36:16 -0700

    expanded html to include more pages. Updated css to match and added javascript funcitons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // selecting elements 
-const menu = document.querySelector("#menu")
+const menu = document.querySelector("#menu");
 const start = document.querySelector("#startQuiz");
 const quiz = document.querySelector("#quiz");
 const question = document.querySelector("#question");
@@ -7,6 +7,17 @@ const choiceA = document.querySelector("#A");
 const choiceB = document.querySelector("#B");
 const choiceC = document.querySelector("#C");
 const choiceD = document.querySelector("#D");
+const results = document.querySelector("#results");
+const submit = document.querySelector("#submit");
+const scores = document.querySelector("#scores");
+const back = document.querySelector("#back");
+const clear = document.querySelector("#clear");
+let time = document.querySelector(".time");
+
+let runningQuestion = 0;
+var totalSeconds = 0;
+var secondsElapsed = 0;
+var interval;
 
 // creating questions
 let questions = [
@@ -53,18 +64,23 @@ let questions = [
     
 ]
 
-const lastQuestion = questions.length -1;
-let runningQuestion = 0;
+let timeleft = 60;
+let timer = setInterval(function() {
+    if (timeleft < 0) {
+        clearInterval(timer);
+        endResults();
+    } else {
+        document.querySelector(".time").innerHTML = "Time: " + timeleft;
+    }
+    timeleft -= 1;
+}, 1000);
 
+function stopTimer() {
+    clearInterval(timer);
+}
 
-choiceA.addEventListener("click", produceQuestion);
-choiceB.addEventListener("click", produceQuestion);
-choiceC.addEventListener("click", produceQuestion);
-choiceD.addEventListener("click", produceQuestion);
-
-function produceQuestion() {
+function renderQuestion() {
     let q = questions[runningQuestion];
-
     question.innerHTML = q.question;
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
@@ -72,19 +88,57 @@ function produceQuestion() {
     choiceD.innerHTML = q.choiceD;
 }
 
-start.addEventListener("click", startQuiz);
-
 function startQuiz() {
     menu.style.display = "none";
-    produceQuestion();
+    setInterval(timer)
+    renderQuestion();
     quiz.style.display = "block";
 }
 
-function checkAnswer() {
-    if(runningQuestion < lastQuestion) {
-        runningQuestion++
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].correct) {
+        
     }
+    if (runningQuestion < questions.length -1) {
+        runningQuestion++;
+    } else {
+        stopTimer();
+        quiz.style.display = "none";
+        results.style.display = "block";
+        
+    }
+}
+
+function endResults() {
+    results.style.display = "block";
+    menu.style.display = "none";
+    quiz.style.display = "none";
+    scores.style.display = "none";
+}
+
+function submitResults() {
+    results.style.display = "none";
+    menu.style.display = "none";
+    quiz.style.display = "none";
+    scores.style.display = "block";
+}
+
+function goBack() {
+    scores.style.display = "none";
+    menu.style.display = "block";
+}
+
+function clearHighscores() {
 
 }
 
 
+
+start.addEventListener("click", startQuiz);
+choiceA.addEventListener("click", renderQuestion);
+choiceB.addEventListener("click", renderQuestion);
+choiceC.addEventListener("click", renderQuestion);
+choiceD.addEventListener("click", renderQuestion);
+submit.addEventListener("click", submitResults);
+back.addEventListener("click", goBack);
+clear.addEventListener("click", clearHighscores);
